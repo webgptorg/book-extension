450cb6803b49ee5c697f2ec8199c06f28bcfd813
Pavol Hejny <me@pavolhejny.com>
2025-05-24 22:33:58 +0200

    🔑 Update license information - Business Source License 1.1 (BUSL-1.1)

--- batch-project-editor.js
+++ batch-project-editor.js
@@ -1,5 +1,5 @@
 // Note: This file is used to configure @hejny `s batch-project-editor rules
 //       @see https://github.com/hejny/batch-project-editor
 module.exports = {
-    ignoreWorkflows: ['contributing','partners', 'license', 'badges', 'auditDependencies'],
+    ignoreWorkflows: ['contributing','partners', 'license', 'badges', 'auditDependencies','description'],
 };
